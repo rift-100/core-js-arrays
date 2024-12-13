1db0a056b8cf438f0ada1cd40d76774ba5025eb0
Sergei Shinkarenko <exp33int@gmail.com>
2024-12-13 23:35:33 +0500

    feat: implement sumArrays func

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -38,8 +38,18 @@ function getIntervalArray(start, end) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  const maxSize = arr1.length > arr2.length ? arr1.length : arr2.length;
+  return Array.from({ length: maxSize }, (_, i) => {
+    let sum = 0;
+    if (arr1[i]) {
+      sum += arr1[i];
+    }
+    if (arr2[i]) {
+      sum += arr2[i];
+    }
+    return sum;
+  });
 }
 
 /**
