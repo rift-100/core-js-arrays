bc1cb35bb6f3e67bf354671cf78fc32f90c063f8
Jegres Ok <exp33int@gmail.com>
2025-01-13 22:41:58 +0500

    feat: implement getHexRGBValues func

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -449,8 +449,13 @@ function getIndicesOfOddNumbers(numbers) {
  *    getHexRGBValues([ 0, 255, 16777215]) => [ '#000000', '#0000FF', '#FFFFFF' ]
  *    getHexRGBValues([]) => []
  */
-function getHexRGBValues(/* arr */) {
-  throw new Error('Not implemented');
+function getHexRGBValues(arr) {
+  return arr.map((el) => {
+    if (el > 256) {
+      return '#FFFFFF';
+    }
+    return `#${el.toString(16).padStart(6, '0').toUpperCase()}`;
+  });
 }
 
 /**
