2acee5d84a55eb90caf2e0e727d64d25fd1b4de0
Jegres Ok <exp33int@gmail.com>
2025-01-13 22:23:31 +0500

    feat: implement getIndicesOfOddNumbers func

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -428,8 +428,15 @@ function getIdentityMatrix(n) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  return numbers
+    .map((el, idx) => {
+      if (el % 2 === 1) {
+        return idx;
+      }
+      return null;
+    })
+    .filter((el) => el !== null);
 }
 
 /**
