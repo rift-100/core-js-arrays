438fc3df0be4a0dc7148ad91f973ca01631716aa
Jegres Ok <exp33int@gmail.com>
2025-01-13 21:54:50 +0500

    feat: implement createChunks func

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -335,8 +335,9 @@ function calculateBalance(arr) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  const numOfSubArrs = Math.ceil(arr.length / chunkSize);
+  return Array.from({ length: numOfSubArrs }, () => arr.splice(0, chunkSize));
 }
 
 /**
